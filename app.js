647a176d03f63ff66ef34635878791357529eb6e
Tim <timotheeaneumann@gmail.com>
2017-01-03 13:09:25 -0800

    Setup Webpack
    Setup with dev-server and react

---
+++ app.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Hello from "./index";
+
+ReactDOM.render(
+  <Hello name="World" />,
+  document.body
+);
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+	entry: "./app.js",
+	output: {
+		filename: "bundle.js"
+	},
+	module: {
+		preLoaders: [
+    	{
+      	test: /\.js$/,
+      	exclude: /node_modules/,
+      	loader: 'jshint-loader'
+			}
+  	],
+  	loaders: [
+    	{
+      	test: [/\.js$/, /\.es6$/],
+      	exclude: /node_modules/,
+      	loader: 'babel-loader',
+      	query: {
+       		presets: ['react', 'es2015'] 
+      	}
+    	}
+  	]
+	},
+	resolve: {
+  	extensions: ['', '.js', '.jsx', '.es6']
+	},
+}
